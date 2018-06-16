22e72864ce48949872f3d24621427a0646928153
Septian A Tama <sfirst.tama@gmail.com>
2018-06-17 04:12:21 +0700

    add about button

--- vue-native-vn/App.vue
+++ vue-native-vn/App.vue
@@ -3,17 +3,47 @@
 </template>
 
 <script>
+import React from 'react';
+import { Text } from 'react-native';
+// import { MaterialIcons } from '@expo/vector-icons';
+
 import { StackNavigator } from 'vue-native-router';
 
 import HomeScreen from './screens/Home.vue';
+import AboutScreen from './screens/About';
 
 const AppNavigation = StackNavigator({
   Home: {
     screen: HomeScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Home - Various News',
+      headerRight: (
+        <Text 
+          onPress={ () => navigation.navigate("About") }
+          style={{
+            padding: 16,
+            color: '#fff',
+          }}
+        >
+          About
+        </Text>
+      ) 
+    }),
+  },
+  About: {
+    screen: AboutScreen,
     navigationOptions: {
-      title: 'Home - Various News'
-    }
-  }
+      title: 'About',
+    },
+  },
+}, {
+  initialRouteName: 'Home',
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: '#2962ff',
+    },
+    headerTintColor: '#fff',
+  },
 });
 
 export default {
---
+++ vue-native-vn/screens/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <view>
+    <text>About Various News - vue-native version<text>
+    <text>By: tamatamvan</Text>
+  </view>
+</template>
