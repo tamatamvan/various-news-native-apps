83549c7931356f05d47e35ba7bd80ec231c309fa
Septian A Tama <sfirst.tama@gmail.com>
2018-06-17 03:46:44 +0700

    styling news card

--- vue-native-vn/components/NewsCard.vue
+++ vue-native-vn/components/NewsCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <view>
+  <view class="card-outer">
     <image
       :source="{ uri: article.urlToImage }"
       :style="{ width: '100%', height: 200 }"
     />
-    <text>{{ article.title }}</text>
+    <text class="card-title">{{ article.title }}</text>
   </view>
 </template>
 
@@ -13,3 +13,17 @@ export default {
   props: [ 'article' ]
 }
 </script>
+
+
+<style>
+.card-outer {
+  background-color: #fff;
+  margin-bottom: 8;
+  elevation: 2;
+}
+.card-title {
+  font-size: 18;
+  font-weight: bold;
+  padding: 4;
+}
+</style>
