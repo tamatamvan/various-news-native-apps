6b72d4db4fa81e0f7f9bae93665ff7e7ac64dc38
Septian A Tama <sfirst.tama@gmail.com>
2018-06-17 04:44:47 +0700

    styling ABout page

--- vue-native-vn/screens/About.vue
+++ vue-native-vn/screens/About.vue
@@ -1,6 +1,18 @@
 <template>
-  <view>
-    <text>About Various News - vue-native version<text>
+  <view class="container">
+    <view>
+      <text>About Various News - vue-native version<text>
+    </view>
     <text>By: tamatamvan</Text>
   </view>
 </template>
+
+
+<style>
+ .container {
+   flex: 1;
+   justify-content: center;
+   align-items: center;
+   flex-direction: column;
+ }
+</style>
