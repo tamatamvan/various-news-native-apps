b0f94e4f9844ba9e24cba3996514a1d5a3f09f0f
Septian A Tama <sfirst.tama@gmail.com>
2018-06-17 03:42:46 +0700

    add padding to container

--- vue-native-vn/screens/Home.vue
+++ vue-native-vn/screens/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <view>
+  <view class="container">
     <news-list/>
   </view>
 </template>
@@ -12,3 +12,9 @@ export default {
   },
 }
 </script>
+
+<style>
+.container {
+  padding: 16;
+}
+</style>
