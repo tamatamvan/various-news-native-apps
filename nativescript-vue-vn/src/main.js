8316400c382c1c605664d7ff24517647b9bdfd9f
Septian A Tama <sfirst.tama@gmail.com>
2018-06-19 01:37:54 +0700

    installing axios

--- nativescript-vue-vn/src/main.js
+++ nativescript-vue-vn/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'nativescript-vue';
+import axios from 'axios';
 
 import router from './router';
 
 
 import './styles.scss';
+
+Vue.prototype.$http = axios;
 
 // Uncommment the following to see NativeScript-Vue output logs
 //Vue.config.silent = false;
