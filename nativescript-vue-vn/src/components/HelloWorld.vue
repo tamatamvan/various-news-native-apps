592fc76bf40f38d8404eb3ba29cedceb60073bc1
Septian A Tama <sfirst.tama@gmail.com>
2018-06-19 01:46:21 +0700

    fetching news from api

--- nativescript-vue-vn/src/components/HelloWorld.vue
+++ nativescript-vue-vn/src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
 
     <StackLayout class="hello-world">
       <Label class="body" textWrap=true text="This is a hello world component, tap the button if you dare"/>
-
+      <!-- <Label :text="this.$router.meta.title"/> -->
       <Button class="btn btn-primary" @tap="surprise = !surprise" text="Tap me!"/>
       <Image v-if="surprise" src="~/images/NativeScript-Vue.png"/>
 
--- nativescript-vue-vn/src/components/Home.vue
+++ nativescript-vue-vn/src/components/Home.vue
@@ -1,10 +1,33 @@
 <template>
   <Page class="page">
-    <ActionBar class="action-bar" title="Home"/>
+    <ActionBar class="action-bar" title="Home - Various News"/>
 
     <StackLayout>
       <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World 123</Button>
     </StackLayout>
-
   </Page>
 </template>
+
+<script>
+export default {
+ data () {
+   return {
+     articles: [],
+   }
+ },
+ methods: {
+   fetchNews () {
+     console.log('HAHAHA')
+     this.$http.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c88e08e6d1754a538e8e00ca712c81e0`)
+      .then(({ data }) => {
+        this.articles = data.articles;
+      })
+      .catch(err => console.log(err));
+   },
+ },
+ mounted () {
+   this.fetchNews();
+ },
+}
+</script>
+
