b1aa4927deb21e55c55b58c4db96ef17a17c6806
Septian A Tama <sfirst.tama@gmail.com>
2018-06-19 01:05:59 +0700

    initiate native script vue project

---
+++ nativescript-vue-vn/src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Home"/>
+
+    <StackLayout>
+      <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World 123</Button>
+    </StackLayout>
+
+  </Page>
+</template>
